cc18ac7bcabb4a2d3adddfabf91300f9c5e6d465
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-07-29 08:13:36 +0000

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -2,28 +2,40 @@ class OwnersController < ApplicationController
 
   get '/owners' do
     @owners = Owner.all
-    erb :'/owners/index' 
+    erb :'/owners/index'
   end
 
-  get '/owners/new' do 
+  get '/owners/new' do
+    @pets = Pet.all
     erb :'/owners/new'
   end
 
-  post '/owners' do 
-    
+  post '/owners' do
+    @owner = Owner.create(params["owner"])
+    @owner.pets << Pet.create(name: params["pet"]["name"]) if !params["pet"]["name"].empty?
+    redirect "/owners/#{@owner.id}"
   end
 
-  get '/owners/:id/edit' do 
+  get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
+    @pets = Pet.all
     erb :'/owners/edit'
   end
 
-  get '/owners/:id' do 
+  get '/owners/:id' do
     @owner = Owner.find(params[:id])
     erb :'/owners/show'
   end
 
-  patch '/owners/:id' do 
-   
+  patch '/owners/:id' do
+    if !params[:owner].keys.include?("pet_ids")
+      params[:owner]["pet_ids"] = []
+    end
+
+    @owner = Owner.find(params[:id])
+    @owner.name = Owner.update(params["owner"])
+
+    @owner.pets << Pet.create(params["pet"]) if !params["pet"]["name"].empty?
+    redirect "owners/#{@owner.id}"
   end
 end

6151618a0192f5897ba9a07d0b293b03af16a8ec
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-07-29 19:27:53 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,7 +11,7 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-    pet = Pet.create(params["pet"])
+    pet = Pet.create(name: params["pet"]["name"])
     pet.owner = Owner.create(name: params["owner"]["name"]) if !params["owner"]["name"].empty?
     pet.save
 

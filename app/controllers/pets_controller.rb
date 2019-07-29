19cbe138dbed255d1bc06423bfe88f3c448e52b2
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-07-29 19:16:00 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -38,7 +38,8 @@ class PetsController < ApplicationController
     pet = Pet.find(params[:id])
     @pet.update(params["pet"])
     @pet.owners << Owner.create(name: params["owner"]["name"]) if !params["owner"]["name"].empty?
-
+    @pet.save
+    
     redirect to "pets/#{@pet.id}"
   end
 end
